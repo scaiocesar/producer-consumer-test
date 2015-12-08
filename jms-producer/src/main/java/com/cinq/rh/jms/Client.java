240fca01f86874d7fec825d9be0c4d092a74d59c
a.kretschmer <ajkret@hotmail.com>
2015-12-08 16:10:15 -0200

    Removed imports.

--- jms-producer/src/main/java/com/cinq/rh/jms/Client.java
+++ jms-producer/src/main/java/com/cinq/rh/jms/Client.java
@@ -1,18 +1,5 @@
 package com.cinq.rh.jms;
 
-import java.util.Properties;
-
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.DeliveryMode;
-import javax.jms.Destination;
-import javax.jms.ExceptionListener;
-import javax.jms.JMSException;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-import javax.naming.Context;
-import javax.naming.InitialContext;
 
 import org.springframework.stereotype.Component;
 
@@ -20,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class Client  {
 
 	public boolean sendAMessage(String greeting) {
-		// Send the message 
+		// Send the message
 	}
 
 
